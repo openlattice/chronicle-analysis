7125a48262fd97fa1496abeed0fa3aec1db34e1d
jokedurnez <joke.durnez@gmail.com>
2019-09-27 16:12:41 -0700

    better testing for summary and preprocessing

--- chroniclepy/test/check_output.py
+++ chroniclepy/test/check_output.py
@@ -11,25 +11,51 @@ def get_parser():
 
 def main():
     opts = get_parser().parse_args()
-    expected_file = os.path.join(
-        opts.directory, 
-        'resources/constants/constants.yaml'
-    )
-    encountered_file =  os.path.join(
-        opts.directory, 
-        'resources/output/summary_hourly.csv'
-    )
     
-    with open(expected_file, 'r') as outfile:
-        expected_output = yaml.safe_load(outfile)
+    # preprocessed
+    files = {
+        "preprocessing": {
+            "expected": os.path.join(
+                opts.directory,
+                'resources/constants/constants_preprocessed.yaml'
+            ), 
+            "encountered": os.path.join(
+                opts.directory, 
+                'resources/preprocessed/ChronicleData_preprocessed-TestParticipant.csv'
+            ),
+            "folder": os.path.join(opts.directory, "resources/preprocessed")
+        },
+        "summarising": {
+            "expected": os.path.join(
+                opts.directory,
+                'resources/constants/constants_summary.yaml'
+            ), 
+            "encountered": os.path.join(
+                opts.directory, 
+                'resources/output/summary_daily.csv'
+            ),
+            "folder": os.path.join(opts.directory, "resources/output")
+        }
+    }
+        
+    for keyword in files.keys():
+        
+        print("Testing pipeline for %s"%keyword)
 
-    encountered_output = pd.read_csv(encountered_file).to_dict()
-    if not encountered_output == expected_output:
-        raise ValueError("This isn't right !")
-    else:
-        print("Test successful...")
-        shutil.rmtree(os.path.join(opts.directory, "resources/preprocessed"))
-        shutil.rmtree(os.path.join(opts.directory, "resources/output"))
+        with open(files[keyword]['expected'], 'r') as outfile:
+            expected_output = yaml.safe_load(outfile)
+
+        encountered_output = pd.read_csv(files[keyword]['encountered']).to_dict()
+        if not encountered_output == expected_output:
+            print("    There are files with different values !!")
+            for key, value in expected_output.items():
+                if value != encountered_output[key]:
+                    print("        Difference for %s: %s vs. %s"%(key, str(value[0]), str(encountered_output[key][0])))
+
+            raise ValueError("    This isn't right !")
+        else:
+            print("    Test successful...")
+            shutil.rmtree(files[keyword]['folder'])
     
 if __name__ == '__main__':
     main()
--- chroniclepy/test/end-to-end.sh
+++ chroniclepy/test/end-to-end.sh
@@ -2,17 +2,18 @@ FOLDER=$(pwd)
 
 docker run \
   -v $FOLDER:$FOLDER \
-  openlattice/chroniclepy:v1.2 \
+  openlattice/chroniclepy:v1.3-rc1 \
   all \
   $FOLDER/resources/rawdata \
   $FOLDER/resources/preprocessed \
   $FOLDER/resources/subsetted \
-  $FOLDER/resources/output
+  $FOLDER/resources/output \
+  --precision 3600
 
 docker run \
   -v $FOLDER:$FOLDER \
   --entrypoint python \
-  openlattice/chroniclepy:v1.2 \
+  openlattice/chroniclepy:v1.3-rc1 \
   $FOLDER/check_output.py --directory $FOLDER
 
 
